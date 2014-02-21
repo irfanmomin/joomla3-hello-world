60481e9d74f38457cc53262adb54e73b72dae055
irfan <irfan.momin990@gmail.com>
2014-02-21 10:26:06 +0530

    helloworld component

---
+++ admin/views/helloworlds/tmpl/default_body.php
@@ -0,0 +1,58 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+
+foreach ($this->items as $i => $item)
+{
+	if ($item->level >= 1)
+	{
+		$parentsStr = "";
+		$_currentParentId = $item->parent_id;
+		$parentsStr = " " . $_currentParentId;
+		$v = "";
+
+		for ($i2 = 0; $i2 < $item->level; $i2++)
+		{
+			$v .= "-";
+		}
+
+		$v = $v . " " . $item->title;
+		?>
+		<tr >
+			<td>
+				<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+			</td>
+			<td>
+				<?php echo $item->level; ?>
+			</td>
+			<td>
+				<?php echo $v; ?>
+			</td>
+			<td>
+				<?php echo $item->alias; ?>
+			</td>
+		</tr>
+	<?php
+
+	}
+	else
+	{
+		?>
+	<tr >
+		<td>
+			<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+		</td>
+		<td>
+			<?php echo $item->level; ?>
+		</td>
+		<td>
+			<?php echo $item->title; ?>
+		</td>
+		<td>
+			<?php echo $item->alias; ?>
+		</td>
+	</tr>
+<?php
+	}
+}
---
+++ admin/views/helloworlds/tmpl/default_head.php
@@ -0,0 +1,19 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<tr>
+        <th width="20">
+                <?php echo JHtml::_('grid.checkall'); ?>
+        </th>
+        <th>
+                <?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_HEADING_ID'); ?>
+        </th>
+        <th width="10">
+                <?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_HEADING_TITLE'); ?>
+        </th>
+        <th width="20">
+                <?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_Alias'); ?>
+        </th>
+</tr>
